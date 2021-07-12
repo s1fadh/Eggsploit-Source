4f872bc5a5f76c15e9d3835d6dde3d2d45bf7b15
s1fadh <76829642+s1fadh@users.noreply.github.com>
2021-07-12 20:16:45 +0800

    Add files via upload

---
+++ Egg/Egg/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Egg
+{
+    partial class Egg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Egg));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.scintilla1 = new ScintillaNET.Scintilla();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.executeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(248, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(329, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(416, 241);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Top Most";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 237);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Inject";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // scintilla1
+            // 
+            this.scintilla1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scintilla1.Location = new System.Drawing.Point(6, 6);
+            this.scintilla1.Name = "scintilla1";
+            this.scintilla1.Size = new System.Drawing.Size(342, 225);
+            this.scintilla1.TabIndex = 8;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.executeToolStripMenuItem,
+            this.refreshToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(116, 70);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // executeToolStripMenuItem
+            // 
+            this.executeToolStripMenuItem.Name = "executeToolStripMenuItem";
+            this.executeToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.executeToolStripMenuItem.Text = "Execute";
+            this.executeToolStripMenuItem.Click += new System.EventHandler(this.executeToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(354, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(132, 225);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Egg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 266);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.scintilla1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Egg";
+            this.Text = "Egg";
+            this.Load += new System.EventHandler(this.Egg_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button5;
+        private ScintillaNET.Scintilla scintilla1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem executeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+    }
+}
+
---
+++ Egg/Egg/Form1.cs
@@ -0,0 +1,184 @@
+﻿using ScintillaNET;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Egg
+{
+    public partial class Egg : Form
+    {
+        #region Variables
+        EasyExploits.Module Eggs = new EasyExploits.Module();
+        #endregion
+
+        public Egg()
+        {
+            InitializeComponent();
+
+            foreach (var dir in new[] { "workspace", "scripts" })
+            {
+                if (Directory.Exists(dir)) continue;
+                Directory.CreateDirectory(dir);
+            }
+
+            RefreshList();
+        }
+
+        #region Events
+        private void Egg_Load(object sender, EventArgs e)
+        {
+            #region Syntax
+            // Extracted from the Lua Scintilla lexer and SciTE .properties file
+
+            var alphaChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var numericChars = "0123456789";
+            var accentedChars = "ŠšŒœŸÿÀàÁáÂâÃãÄäÅåÆæÇçÈèÉéÊêËëÌìÍíÎîÏïÐðÑñÒòÓóÔôÕõÖØøÙùÚúÛûÜüÝýÞþßö";
+
+            // Configuring the default style with properties
+            // we have common to every lexer style saves time.
+            scintilla1.StyleResetDefault();
+            scintilla1.Styles[Style.Default].Font = "Consolas";
+            scintilla1.Styles[Style.Default].Size = 10;
+            scintilla1.StyleClearAll();
+
+            // Configure the Lua lexer styles
+            scintilla1.Styles[Style.Lua.Default].ForeColor = Color.Silver;
+            scintilla1.Styles[Style.Lua.Comment].ForeColor = Color.Green;
+            scintilla1.Styles[Style.Lua.CommentLine].ForeColor = Color.Green;
+            scintilla1.Styles[Style.Lua.Number].ForeColor = Color.Olive;
+            scintilla1.Styles[Style.Lua.Word].ForeColor = Color.Blue;
+            scintilla1.Styles[Style.Lua.Word2].ForeColor = Color.BlueViolet;
+            scintilla1.Styles[Style.Lua.Word3].ForeColor = Color.DarkSlateBlue;
+            scintilla1.Styles[Style.Lua.Word4].ForeColor = Color.DarkSlateBlue;
+            scintilla1.Styles[Style.Lua.String].ForeColor = Color.Red;
+            scintilla1.Styles[Style.Lua.Character].ForeColor = Color.Red;
+            scintilla1.Styles[Style.Lua.LiteralString].ForeColor = Color.Red;
+            scintilla1.Styles[Style.Lua.StringEol].BackColor = Color.Pink;
+            scintilla1.Styles[Style.Lua.Operator].ForeColor = Color.Purple;
+            scintilla1.Styles[Style.Lua.Preprocessor].ForeColor = Color.Maroon;
+            scintilla1.Lexer = Lexer.Lua;
+            scintilla1.WordChars = alphaChars + numericChars + accentedChars;
+
+            // Console.WriteLine(scintilla.DescribeKeywordSets());
+
+            // Keywords
+            scintilla1.SetKeywords(0, "and break do else elseif end for function if in local nil not or repeat return then until while" + " false true" + " goto");
+            // Basic Functions
+            scintilla1.SetKeywords(1, "assert collectgarbage dofile error _G getmetatable ipairs loadfile next pairs pcall print rawequal rawget rawset setmetatable tonumber tostring type _VERSION xpcall string table math coroutine io os debug" + " getfenv gcinfo load loadlib loadstring require select setfenv unpack _LOADED LUA_PATH _REQUIREDNAME package rawlen package bit32 utf8 _ENV");
+            // String Manipulation & Mathematical
+            scintilla1.SetKeywords(2, "string.byte string.char string.dump string.find string.format string.gsub string.len string.lower string.rep string.sub string.upper table.concat table.insert table.remove table.sort math.abs math.acos math.asin math.atan math.atan2 math.ceil math.cos math.deg math.exp math.floor math.frexp math.ldexp math.log math.max math.min math.pi math.pow math.rad math.random math.randomseed math.sin math.sqrt math.tan" + " string.gfind string.gmatch string.match string.reverse string.pack string.packsize string.unpack table.foreach table.foreachi table.getn table.setn table.maxn table.pack table.unpack table.move math.cosh math.fmod math.huge math.log10 math.modf math.mod math.sinh math.tanh math.maxinteger math.mininteger math.tointeger math.type math.ult" + " bit32.arshift bit32.band bit32.bnot bit32.bor bit32.btest bit32.bxor bit32.extract bit32.replace bit32.lrotate bit32.lshift bit32.rrotate bit32.rshift" + " utf8.char utf8.charpattern utf8.codes utf8.codepoint utf8.len utf8.offset");
+            // Input and Output Facilities and System Facilities
+            scintilla1.SetKeywords(3, "coroutine.create coroutine.resume coroutine.status coroutine.wrap coroutine.yield io.close io.flush io.input io.lines io.open io.output io.read io.tmpfile io.type io.write io.stdin io.stdout io.stderr os.clock os.date os.difftime os.execute os.exit os.getenv os.remove os.rename os.setlocale os.time os.tmpname" + " coroutine.isyieldable coroutine.running io.popen module package.loaders package.seeall package.config package.searchers package.searchpath" + " require package.cpath package.loaded package.loadlib package.path package.preload");
+
+            // Instruct the lexer to calculate folding
+            scintilla1.SetProperty("fold", "1");
+            scintilla1.SetProperty("fold.compact", "1");
+
+            // Configure a margin to display folding symbols
+            scintilla1.Margins[2].Type = MarginType.Symbol;
+            scintilla1.Margins[2].Mask = Marker.MaskFolders;
+            scintilla1.Margins[2].Sensitive = true;
+            scintilla1.Margins[2].Width = 2;
+
+            // Set colors for all folding markers
+            for (int i = 25; i <= 31; i++)
+            {
+                scintilla1.Markers[i].SetForeColor(SystemColors.ControlLightLight);
+                scintilla1.Markers[i].SetBackColor(SystemColors.ControlDark);
+            }
+
+            // Configure folding markers with respective symbols
+            scintilla1.Markers[Marker.Folder].Symbol = MarkerSymbol.BoxPlus;
+            scintilla1.Markers[Marker.FolderOpen].Symbol = MarkerSymbol.BoxMinus;
+            scintilla1.Markers[Marker.FolderEnd].Symbol = MarkerSymbol.BoxPlusConnected;
+            scintilla1.Markers[Marker.FolderMidTail].Symbol = MarkerSymbol.TCorner;
+            scintilla1.Markers[Marker.FolderOpenMid].Symbol = MarkerSymbol.BoxMinusConnected;
+            scintilla1.Markers[Marker.FolderSub].Symbol = MarkerSymbol.VLine;
+            scintilla1.Markers[Marker.FolderTail].Symbol = MarkerSymbol.LCorner;
+
+            // Enable automatic folding
+            scintilla1.AutomaticFold = (AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change);
+            #endregion
+        }
+
+        private void RefreshList()
+        {
+            listBox1.Items.Clear();
+
+            foreach (var script in Directory.GetFiles("./scripts", "*.lua"))
+                listBox1.Items.Add(Path.GetFileName(script));
+
+            foreach (var script in Directory.GetFiles("./scripts", "*.txt"))
+                listBox1.Items.Add(Path.GetFileName(script));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Eggs.ExecuteScript(scintilla1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var input = MessageBox.Show("Are you sure want to clear Text? All changes will be lost!",
+                    "Egg", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (input == DialogResult.Yes) scintilla1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenDialog = new OpenFileDialog()
+            {
+                Title = "Open File",
+                Filter = "Txt Files (*.txt)|*.txt|Lua Files (*.lua)|*.lua",
+                FilterIndex = 2,
+            };
+
+            if (OpenDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                scintilla1.Text = File.ReadAllText(OpenDialog.FileName);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveDialog = new SaveFileDialog()
+            {
+                Title = "Save File",
+                Filter = "Txt Files (*.txt)|*.txt|Lua Files (*.lua)|*.lua",
+                FilterIndex = 2,
+            };
+
+            if (SaveDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                File.WriteAllText(SaveDialog.FileName, scintilla1.Text);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Eggs.LaunchExploit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost ^= true;
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            scintilla1.Text = File.ReadAllText("./scripts/" + listBox1.SelectedItem.ToString());
+        }
+
+        private void executeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Eggs.ExecuteScript(scintilla1.Text);
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+        #endregion
+    }
+}
